63ae82eaedced1f2673c3aa3a726a9fd942e4823
SangwanYu <ryusangwan12@gmail.com>
2026-09-08 09:51:49 +0900

    feat(web): 전역 메뉴 이동 및 로그아웃 확인 연결

--- web/src/components/sidebar/components/SidebarFooter.tsx
+++ web/src/components/sidebar/components/SidebarFooter.tsx
@@ -1,3 +1,4 @@
+import Modal from "@/components/common/Modal";
 import { footerMenuItems } from "../constants/menuItems";
 import { useSidebarNavigation } from "../hooks/useSidebarNavigation";
 import { MenuItem } from "./MenuItem";
@@ -7,19 +8,48 @@ interface SidebarFooterProps {
 }
 
 export const SidebarFooter = ({ isOpen }: SidebarFooterProps) => {
-  const { handleMenuClick, isActive } = useSidebarNavigation();
+  const {
+    handleMenuClick,
+    isActive,
+    isLogoutModalOpen,
+    isLoggingOut,
+    closeLogoutModal,
+    confirmLogout,
+    logoutWarningMessage,
+  } = useSidebarNavigation();
 
   return (
-    <div className="flex flex-col gap-2">
-      {footerMenuItems.map((item) => (
-        <MenuItem
-          key={item.id}
-          item={item}
-          isOpen={isOpen}
-          isActive={isActive(item)}
-          onClick={handleMenuClick}
-        />
-      ))}
-    </div>
+    <>
+      <div className="flex flex-col gap-2">
+        {footerMenuItems.map((item) => (
+          <MenuItem
+            key={item.id}
+            item={item}
+            isOpen={isOpen}
+            isActive={isActive(item)}
+            onClick={handleMenuClick}
+          />
+        ))}
+      </div>
+      {isLogoutModalOpen && (
+        <Modal
+          title="로그아웃"
+          onClose={closeLogoutModal}
+          primaryLabel={
+            logoutWarningMessage
+              ? "다시 시도"
+              : isLoggingOut
+                ? "로그아웃 중..."
+                : "로그아웃"
+          }
+          onPrimaryClick={confirmLogout}
+          isPrimaryDisabled={isLoggingOut}
+        >
+          <p className="typo-body4 text-(--color-text-secondary)">
+            {logoutWarningMessage ?? "현재 계정에서 로그아웃하시겠습니까?"}
+          </p>
+        </Modal>
+      )}
+    </>
   );
 };
